4bead2bfcdade202ce3883a8672dd9048e4bc589
Winlypse <nts3@o2.pl>
2024-10-22 23:38:33 +0200

    Changes!

--- src/main/kotlin/winlyps/noSpectatorsChunks/NoSpectatorsChunks.kt
+++ src/main/kotlin/winlyps/noSpectatorsChunks/NoSpectatorsChunks.kt
@@ -6,9 +6,14 @@ class NoSpectatorsChunks : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
+        server.worlds.forEach { world ->
+            world.setGameRuleValue("spectatorsGenerateChunks", "false")
+        }
+        logger.info("NoSpectatorsChunks plugin has been enabled.")
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+        logger.info("NoSpectatorsChunks plugin has been disabled.")
     }
 }

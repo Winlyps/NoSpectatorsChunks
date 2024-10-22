20f2e005f8e13f312bddf7c3c8c183f1865b9bf4
Winlypse <nts3@o2.pl>
2024-10-22 23:38:23 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/noSpectatorsChunks/NoSpectatorsChunks.kt
@@ -0,0 +1,14 @@
+package winlyps.noSpectatorsChunks
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoSpectatorsChunks : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
